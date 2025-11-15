f87b4397aeae30c84c99e44c1f26a9cfb9b521e3
Ori Shamir <orishamir04@gmail.com>
2025-11-15 13:24:53 +0200

    Improve ci

--- src/pvz_sdk/toggleables/instant_recharge.rs
+++ src/pvz_sdk/toggleables/instant_recharge.rs
@@ -4,7 +4,8 @@ use crate::Popcapgame;
 const INSTRUCTION_OFFSETS: [usize; 1] = [0x6CB1FD];
 
 /// ```diff
-/// - GameAssembly.dll+6CB1FD - 7E 1A                 - jle GameAssembly.dll+6CB219
+///   GameAssembly.dll+6CB1FA - 3B 43 74              - cmp slot->reloadCounter+1, slot->reloadDestination
+/// - GameAssembly.dll+6CB1FD - 7E 1A                 - jle GameAssembly.dll+6CB219   (If hasn't reached reload destination, reload didn't finish)
 /// + GameAssembly.dll+6CB1FD - 90 90                 - nop
 /// ```
 pub struct InstantRechargeCheat {}
